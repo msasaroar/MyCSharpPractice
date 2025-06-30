249d97ce1304546c214eb06be8780f788ae91c2a
Azfar Rabib Sakif <151199960+msasaroar@users.noreply.github.com>
2025-06-30 22:06:51 +0600

    1

---
+++ MyDemoClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms; // Needed for MessageBox
+
+public class MyDemoClass
+{
+    // Field
+    private string message;
+
+    // Property
+    public int Number { get; set; }
+
+    // Default Constructor
+    public MyDemoClass()
+    {
+        message = "Hello from MyDemoClass!";
+        Number = 0;
+    }
+
+    // Parameterized Constructor
+    public MyDemoClass(string msg, int num)
+    {
+        message = msg;
+        Number = num;
+    }
+
+    // Method
+    public void ShowMessage()
+    {
+        MessageBox.Show($"Message: {message}\nNumber: {Number}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyCSharpPractice
+{
+    // Program Entry Point
+    public static class Program
+    {
+        [STAThread] // Required for MessageBox
+        public static void Main()
+        {
+            MyDemoClass demo1 = new MyDemoClass();
+            demo1.ShowMessage();
+
+            MyDemoClass demo2 = new MyDemoClass("This is a custom message!", 42);
+            demo2.ShowMessage();
+        }
+    }
+}
